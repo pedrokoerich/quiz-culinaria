c8fd046da694ff93f14adfa096b7d3372fbe3a7c
Pedro Koerich <pedro.koerich@totvs.com.br>
2024-11-29 17:14:22 -0300

    feat: finalizada primeira página do quiz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { QuestionItem } from "@/components/QuestionItem";
 import { questions } from "@/data/questions";
 import { useState } from "react";
 
 const Page = () => {
   const title = "Quiz de Culinária"
   const [currenteQuestion, setCurrenteQuestion] = useState(0);
+  const handleAnswer = (answer: number) => {
+  
+  }
 
     return (
       <div className="w-full h-screen flex justify-center items-center bg-blue-950">
@@ -13,7 +17,13 @@ const Page = () => {
             <div className="p-5 font-bold text-2xl shadow border-b border-gray-300">
               {title}
             </div>
-            <div className="p-5"></div>
+            <div className="p-5">
+              <QuestionItem 
+                question={questions[currenteQuestion]}
+                count={currenteQuestion + 1}
+                onAnswer={handleAnswer}
+              />
+            </div>
             <div className="p-5 text-center border-t border-gray-300">
               {currenteQuestion + 1} de {questions.length} pergunta{questions.length > 1 ? "s" : ""}
             </div>
